a1e7463e2301acc7d9dbbc1c6750fdf7f816dbcd
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2026-01-08 21:48:15 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* basename ayarını ekliyoruz. Vite bunu build komutundan alacak. */}
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
